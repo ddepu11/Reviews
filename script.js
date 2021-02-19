578b85a5c7d1957f513517e14bd314074cd9630b
Deepanshu Tiwari <ddepu11@gmail.com>
2021-02-19 10:48:20 +0530

    Completed the Review page project

---
+++ script.js
@@ -0,0 +1,49 @@
+import people from "./src/people.js";
+
+const displayPic = document.getElementById("dp");
+const nameEl = document.querySelector(".name");
+const positionEl = document.querySelector(".position");
+const previousBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const randomBtn = document.querySelector(".random");
+const descriptionEl = document.getElementById("description");
+
+let counter = 0;
+
+// Put all the info in the dom
+const showInfoInDom = (dp, name, position, description) => {
+  displayPic.setAttribute("src", dp);
+  nameEl.innerText = name;
+  positionEl.innerText = position;
+  descriptionEl.innerText = description;
+};
+
+// Event Listeners
+previousBtn.addEventListener("click", () => {
+  if (counter < 0) {
+    counter += people.length;
+  }
+  const { dp, name, position, description } = people[counter];
+
+  showInfoInDom(dp, name, position, description);
+
+  counter--;
+});
+
+nextBtn.addEventListener("click", () => {
+  if (counter > people.length - 1) {
+    counter = counter - people.length;
+  }
+
+  const { dp, name, position, description } = people[counter];
+  showInfoInDom(dp, name, position, description);
+
+  counter++;
+});
+
+randomBtn.addEventListener("click", () => {
+  const person = people[Math.floor(Math.random() * people.length)];
+
+  const { dp, name, position, description } = person;
+  showInfoInDom(dp, name, position, description);
+});
---
+++ src/people.js
@@ -0,0 +1,52 @@
+const people = [
+  {
+    name: "Mohan Singh",
+    position: "Manager",
+    dp:
+      "https://images.unsplash.com/photo-1610956044066-705bc798dc6c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    description:
+      "The manager is an employee who is responsible for planning, directing, and overseeing the operations and fiscal health of a business unit, division, department, or operating unit within an organization. The manager is responsible for overseeing and leading the work of a group of people in many instances.",
+  },
+  {
+    name: "Arijit Singh",
+    position: "Designer",
+    dp:
+      "https://images.unsplash.com/photo-1613419441661-6a5af1751d30?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    description:
+      "Graphic Designer Job Responsibilities: Prepares work to be accomplished by gathering information and materials. Plans concept by studying information and materials. Illustrates concept by designing rough layout of art and copy regarding arrangement, size, type size and style, and .",
+  },
+  {
+    name: "Arpit Vishvkarma",
+    position: "Backend Developer",
+    dp:
+      "https://images.unsplash.com/photo-1612277832417-7f3146075560?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+    description:
+      "A back-end web developer is responsible for server-side web application logic and integration of the work front-end developers do. Back-end developers are usually write the web services and APIs used by front-end developers and mobile application developers.",
+  },
+  {
+    name: "Dinesh Sahu",
+    position: "Frontend Developer",
+    dp:
+      "https://images.unsplash.com/photo-1613428792678-087d5d14238b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    description:
+      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application so that a user can see and interact with them directly. The challenge associated with front end development is that the tools and techniques used to create the . ",
+  },
+  {
+    name: "Hariom Agrawal",
+    position: "Chief Financial Officer",
+    dp:
+      "https://images.unsplash.com/photo-1613188665424-b6790816c9f9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    description:
+      "A back-end web developer is responsible for server-side web application logic and integration of the work front-end developers do. Back-end developers are usually write the web services and APIs used by front-end developers and mobile application developers.",
+  },
+  {
+    name: "Bhaskar Nema",
+    position: "Chief Technology Officer",
+    dp:
+      "https://images.unsplash.com/photo-1612817122234-8b4bb9ad7b7c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+    description:
+      "A back-end web developer is responsible for server-side web application logic and integration of the work front-end developers do. Back-end developers are usually write the web services and APIs used by front-end developers and mobile application developers.",
+  },
+];
+
+export default people;
